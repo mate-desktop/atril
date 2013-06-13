387b00d43a02761833a43f5072b9599d8d237e80
Antia Puentes <apuentes@igalia.com>
2013-06-13 18:08:44 +0200

    libview: Avoid to update the current page and scroll to the cursor position, if the caret cursor was not updated
    Move the caret cursor only when requested by the user. Keep the page
    where the caret cursor is in addition to the offset inside that page.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=702068
    origin commit:
    https://git.gnome.org/browse/evince/commit/?h=gnome-3-10&id=4d15ab5

--- libview/ev-view-private.h
+++ libview/ev-view-private.h
@@ -217,6 +217,7 @@ struct _EvView {
 	/* Caret navigation */
 	gboolean caret_enabled;
 	gint     cursor_offset;
+	gint     cursor_page;
 	gboolean cursor_visible;
 	guint    cursor_blink_timeout_id;
 	guint    cursor_blink_time;
