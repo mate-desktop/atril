fd09fb9251212e7161796a2007f2b1f8c97e57a9
raveit65 <chat-to-me@raveit.de>
2016-06-22 10:13:16 +0200

    GTK3 egg: don't use deprecated gdk_threads_{enter/leave} ()

--- cut-n-paste/smclient/eggsmclient-xsmp.c
+++ cut-n-paste/smclient/eggsmclient-xsmp.c
@@ -368,9 +368,13 @@ sm_client_xsmp_startup (EggSMClient *client,
       xsmp->client_id = g_strdup (ret_client_id);
       free (ret_client_id);
 
+#if !GTK_CHECK_VERSION (3, 0, 0)
       gdk_threads_enter ();
+#endif
       gdk_x11_set_sm_client_id (xsmp->client_id);
+#if !GTK_CHECK_VERSION (3, 0, 0)
       gdk_threads_leave ();
+#endif
 
       g_debug ("Got client ID \"%s\"", xsmp->client_id);
     }
@@ -538,7 +542,9 @@ idle_do_pending_events (gpointer data)
   EggSMClientXSMP *xsmp = data;
   EggSMClient *client = data;
 
+#if !GTK_CHECK_VERSION (3, 0, 0)
   gdk_threads_enter ();
+#endif
 
   xsmp->idle = 0;
 
@@ -563,7 +569,9 @@ idle_do_pending_events (gpointer data)
     }
 
  out:
+#if !GTK_CHECK_VERSION (3, 0, 0)
   gdk_threads_leave ();
+#endif
   return FALSE;
 }
 
@@ -1285,9 +1293,13 @@ process_ice_messages (IceConn ice_conn)
 {
   IceProcessMessagesStatus status;
 
+#if !GTK_CHECK_VERSION (3, 0, 0)
   gdk_threads_enter ();
+#endif
   status = IceProcessMessages (ice_conn, NULL, NULL);
+#if !GTK_CHECK_VERSION (3, 0, 0)
   gdk_threads_leave ();
+#endif
 
   switch (status)
     {
