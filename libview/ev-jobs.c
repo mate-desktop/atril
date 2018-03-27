14d976238984f99a1f79f1329ef6d05be6c9f8e3
raveit65 <mate@raveit.de>
2018-03-27 11:20:38 +0200

    libview: fix build without epub
    Fixes https://github.com/mate-desktop/atril/issues/308

--- libview/ev-jobs.c
+++ libview/ev-jobs.c
@@ -858,8 +858,6 @@ snapshot_callback(WebKitWebView *webview,
 	gtk_widget_destroy (gtk_widget_get_toplevel (GTK_WIDGET (webview)));
 }
 
-#endif  /* ENABLE_EPUB */
-
 static void
 web_thumbnail_get_screenshot_cb (WebKitWebView  *webview,
                                  WebKitLoadEvent event,
@@ -891,6 +889,8 @@ webview_load_failed_cb (WebKitWebView  *webview,
 	gtk_widget_destroy (gtk_widget_get_toplevel (GTK_WIDGET (webview)));
 	return TRUE;
 }
+
+#endif  /* ENABLE_EPUB */
 
 static gboolean
 ev_job_thumbnail_run (EvJob *job)
