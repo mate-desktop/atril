ecd0d8cbf4851d36f2fe859b38740d0203949c4d
Martin Wimpress <code@flexion.org>
2019-11-04 18:57:50 +0000

    libview: Reset `pressed_button` when starting Drag and Drop
    If view->pressed_button is left set, when the Drag and Drop operation completes, Atril will act as if it is still in a selection event.
    
    Adapted from: https://gitlab.gnome.org/GNOME/evince/commit/92828bb797742e04aadbfdd62ba1da36837c37cf

--- libview/ev-view.c
+++ libview/ev-view.c
@@ -4807,6 +4807,7 @@ ev_view_motion_notify_event (GtkWidget      *widget,
 					                 event->y);
 
 			view->selection_info.in_drag = FALSE;
+			view->pressed_button = -1;
 
 			gtk_target_list_unref (target_list);
 
@@ -4829,6 +4830,7 @@ ev_view_motion_notify_event (GtkWidget      *widget,
 					                 event->y);
 
 			view->image_dnd_info.in_drag = FALSE;
+			view->pressed_button = -1;
 
 			gtk_target_list_unref (target_list);
 
