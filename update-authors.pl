a4f8d785f4c7e0fcec212e5ea4d90fc919ed3efc
rbuj <robert.buj@gmail.com>
2020-02-15 15:54:30 -0500

    Update authors

--- update-authors.pl
+++ update-authors.pl
@@ -44,6 +44,8 @@ sub ReplaceAuthors {
   $_ eq 'rbuj <robert.buj%gmail.com>' and $_ = 'Robert Buj <robert.buj%gmail.com>' for @authors;
   $_ eq 'Scott Balneaves <sbalneav%ltsp.org>' and $_ = 'Scott Balneaves <sbalneav%mate-desktop.org>' for @authors;
   $_ eq 'Wolfgang Ulbrich <chat-to-me%raveit.de>' and $_ = 'Wolfgang Ulbrich <mate%raveit.de>' for @authors;
+  $_ eq 'Wu Xiaotian <yetist%gmail.com>' and $_ = 'Wu Xiaotian <xiaotian.wu%i-soft.com.cn>' for @authors;
+  $_ eq 'ZenWalker <scow%riseup.net>' and $_ = 'Pablo Barciela <scow%riseup.net>' for @authors;
   return @authors;
 }
 
@@ -67,5 +69,9 @@ sub GetNewAuthors {
 
 my @A = GetCurrentAuthors;
 my @B = GetNewAuthors;
+for (@B) {
+  s/<\d+\+(.+?)%users\.noreply\.github\.com>/<$1%users\.noreply\.github\.com>/;
+  s/<(.+?)%users\.noreply\.github\.com>/https:\/\/github.com\/$1/;
+}
 my @merged = sort { $a cmp $b } keys %{{map {($_ => 1)} (@A, @B)}};
 print join(';',@merged) . ';';
